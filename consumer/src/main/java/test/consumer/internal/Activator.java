10bc762cb5aff8f13d8d2e15c600ec8a35cf6cd3
Andreas Pieber <anpieber@gmail.com>
2013-02-21 13:26:07 +0100

    First successful usage of the whiteboard pattern

--- consumer/src/main/java/test/consumer/internal/Activator.java
+++ consumer/src/main/java/test/consumer/internal/Activator.java
@@ -3,26 +3,39 @@ package test.consumer.internal;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 import test.api.DoSomething;
 
 public class Activator implements BundleActivator {
 
+	ServiceTracker<DoSomething, DoSomething> tracker;
+
 	public void start(BundleContext context) throws Exception {
 		System.out.println("started");
-		ServiceReference<DoSomething> somethingService = context
-				.getServiceReference(DoSomething.class);
-		if (somethingService == null) {
-			System.out.println("nope :-(");
-			return;
-		}
-		DoSomething doSomethingService = context.getService(somethingService);
-		System.out.println(doSomethingService.now());
-		context.ungetService(somethingService);
+		tracker = new ServiceTracker<DoSomething, DoSomething>(context,
+				DoSomething.class, null) {
+			@Override
+			public DoSomething addingService(
+					ServiceReference<DoSomething> reference) {
+				DoSomething service = super.addingService(reference);
+				System.out.println(service.now());
+				return service;
+			}
+
+			@Override
+			public void removedService(ServiceReference<DoSomething> reference,
+					DoSomething service) {
+				System.out.println("entfernt");
+				super.removedService(reference, service);
+			}
+		};
+		tracker.open();
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		System.out.println("stoped");
+		tracker.close();
 	}
 
 }
